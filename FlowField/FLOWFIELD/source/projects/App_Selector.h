4a51cd3b8d7b3559be62d500ff9f6a26d70e8191
clovenge <osmanurafi@gmail.com>
2023-01-12 20:51:36 +0100

    Update App_Selector.h

--- FlowField/FLOWFIELD/source/projects/App_Selector.h
+++ FlowField/FLOWFIELD/source/projects/App_Selector.h
@@ -2,51 +2,13 @@
 // Here we determine which application is currently active
 // Create the define here using the "ActiveApp_..." convention and set up the correct include and typedef in the #ifdef below.
 
-//#define ActiveApp_Sandbox
-//#define ActiveApp_Steering
-//#define ActiveApp_CombinedSteering
-//#define ActiveApp_Flocking
-//#define ActiveApp_Graph
 #define ActiveApp_AStar
-//#define ActiveApp_NavMeshGraph
-
-
 
 //---------- Registered Applications -----------
-#ifdef ActiveApp_Sandbox
-#include "projects/Movement/Sandbox/App_Sandbox.h"
-typedef App_Sandbox CurrentApp;
-#endif
-
-#ifdef ActiveApp_Steering
-#include "projects/Movement/SteeringBehaviors/Steering/App_SteeringBehaviors.h"
-typedef App_SteeringBehaviors CurrentApp;
-#endif
-
-#ifdef ActiveApp_CombinedSteering
-#include "projects/Movement/SteeringBehaviors/CombinedSteering/App_CombinedSteering.h"
-typedef App_CombinedSteering CurrentApp;
-#endif
-
-#ifdef ActiveApp_Flocking
-#include "projects/Movement/SteeringBehaviors/Flocking/App_Flocking.h"
-typedef App_Flocking CurrentApp;
-#endif
-
-#ifdef ActiveApp_Graph
-#include "projects/Movement/pathfinding/GraphTheory/App_GraphTheory.h"
-typedef App_GraphTheory CurrentApp;
-#endif
-
 #ifdef ActiveApp_AStar
 #include "projects/Movement/Pathfinding/FlowField/App_PathfindingFlowField/App_PathfindingFlowField.h"
 typedef App_PathfindingFlowField CurrentApp;
 #endif
-
-#ifdef ActiveApp_NavMeshGraph
-#include "projects/Movement/Pathfinding/NavMeshGraph/App_NavMeshGraph.h"
-typedef App_NavMeshGraph CurrentApp;
-#endif 
 
 
 
